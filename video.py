3a9dd51ceafb4f87b5b020b4e6828f5de5658aa9
Fangge0229 <1480754003@qq.com>
2026-03-16 08:35:49 +0800

    add the code of generate train data

---
+++ video.py
@@ -0,0 +1,68 @@
+import cv2, os, sys
+import numpy as np
+from HccePose.bop_loader import bop_dataset
+from HccePose.tester import Tester
+
+if __name__ == '__main__':
+    
+    sys.path.insert(0, os.getcwd())
+    current_dir = os.path.dirname(sys.argv[0])
+    dataset_path = os.path.join(current_dir, 'demo-bin-picking')
+    test_video_path = os.path.join(current_dir, 'test_videos')
+    bop_dataset_item = bop_dataset(dataset_path)
+    obj_id = 1
+    CUDA_DEVICE = '0'
+    # show_op = False
+    show_op = True
+    
+    Tester_item = Tester(bop_dataset_item, show_op = show_op, CUDA_DEVICE=CUDA_DEVICE)
+    for name in ['VID_20251009_141247']:
+        file_name = os.path.join(test_video_path, '%s.mp4'%name)
+        cap = cv2.VideoCapture(file_name)
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out_1 = None
+        out_2 = None
+        cam_K = np.array([
+            [1.63235512e+03, 0.00000000e+00, 9.74032712e+02],
+            [0.00000000e+00, 1.64159967e+03, 5.14229781e+02],
+            [0.00000000e+00, 0.00000000e+00, 1.00000000e+00],
+        ])
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            results_dict = Tester_item.predict(cam_K, frame, [obj_id],
+                                                            conf = 0.85, confidence_threshold = 0.85)
+            fps_hccepose = 1 / results_dict['time']
+            show_6D_vis1 = results_dict['show_6D_vis1']
+            show_6D_vis1[show_6D_vis1 < 0] = 0
+            show_6D_vis1[show_6D_vis1 > 255] = 255
+            if out_1 is None:
+                out_1 = cv2.VideoWriter(
+                    file_name.replace('.mp4', '_show_1.mp4'),
+                    fourcc,
+                    fps,
+                    (show_6D_vis1.shape[1], show_6D_vis1.shape[0])
+                )
+            out_1.write(show_6D_vis1.astype(np.uint8))
+            show_6D_vis2 = results_dict['show_6D_vis2']
+            show_6D_vis2[show_6D_vis2 < 0] = 0
+            show_6D_vis2[show_6D_vis2 > 255] = 255
+            if out_2 is None:
+                out_2 = cv2.VideoWriter(
+                    file_name.replace('.mp4', '_show_2.mp4'),
+                    fourcc,
+                    fps,
+                    (show_6D_vis2.shape[1], show_6D_vis2.shape[0])
+                )
+            cv2.putText(show_6D_vis2, "FPS: {0:.2f}".format(fps_hccepose), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4, cv2.LINE_AA)
+            out_2.write(show_6D_vis2.astype(np.uint8))
+        cap.release()
+        out_1.release()
+        out_2.release()
+    pass
