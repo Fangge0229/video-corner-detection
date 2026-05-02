4d46864008221ac6ddc8955ae390a6ecc0152982
Fangge0229 <1480754003@qq.com>
2026-05-02 23:03:55 +0800

    fixed some problems in model.py

--- model.py
+++ model.py
@@ -5,9 +5,10 @@ import torchvision.models as models
 
 class CornerHead(nn.Module):
     def __init__(self, in_dim=256):
+        super().__init__()
         self.linear1 = nn.Linear(in_channels=256,out_channels=128)
-        self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(in_channels=128,out_channels=8)
+        self.relu = nn.ReLU(inplace=True)
+        self.linear2 = nn.Linear(in_channels=128,out_channels=16)
 
         nn.init.normal_(self.linear1.weight,std=0.01,mean=0.0)
         nn.init.constant_(self.linear1.bias,0)
@@ -18,9 +19,9 @@ class CornerHead(nn.Module):
         x = self.linear1(x)
         x = self.relu(x)
         x = self.linear2(x)
-        return x
+        return x.view(-1, 8, 2)
 
-class ConfidenseHead(nn.Module):
+class ConfidenceHead(nn.Module):
     def __init__(self, in_dim=256):
         self.linear1 = nn.Linear(in_channels=256,out_channels=128)
         self.relu = nn.ReLU()
@@ -41,7 +42,12 @@ class TemporalTransformer(nn.Module):
     def __init__(self, d_model=256, nhead=8, num_layers=4, max_len=16):
         super().__init__()
         self.time_embed = nn.Parameter(torch.randn(max_len, d_model))
-        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=4 * d_model,batch_first=True)
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dim_feedforward=4 * d_model,
+            batch_first=True
+        )
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
     
     def forward(self, x):
@@ -53,6 +59,7 @@ class TemporalTransformer(nn.Module):
 class ROIEncoder(nn.Module):
     def __init__(self, out_dim=256):
         super().__init__()
+        backbone = torchvision.models.resnet18(weights=weight)
         self.backbone = models.resnet18(pretrained=True)
         self.pool = nn.AdaptiveAvgPool2d((1,1))
         self.linear = nn.Linear(in_channels=512,out_channels=out_dim)
@@ -60,7 +67,7 @@ class ROIEncoder(nn.Module):
     def forward(self,x):
         feat = self.backbone(x)
         feat = self.pool(feat)
-        feat = feat.view(feat.shape[0],feaet.shape[1])
+        feat = feat.view(feat.shape[0],-1)#torch.flatten(feat, start_dim=1)有什么区别
         feat = self.linear(feat)
         return feat
 
@@ -76,7 +83,7 @@ class VideoCornerModel(nn.Module):
         B,T,C,H,W = x.shape
         x = x.view(B*T,C,H,W)
         feat = self.roi_encoder(x)
-        feat = feat.view(B,T,feat.shape[1])
+        feat = feat.view(B,T,-1)
         feat = self.temporal_transformer(feat)
         corner_pred = self.corner_head(feat)
         confidence_pred = self.confidence_head(feat)
