422c648f90455cedb06c519fec488c9769f62253
Fangge0229 <1480754003@qq.com>
2026-05-03 22:17:03 +0800

    fix some problems in loss.py

--- loss.py
+++ loss.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-def corner_regression_loss(pred,target):
+def corner_regression_loss(pred, target, target_vis):
     """
     pred_corners:   [B, 8, 2]
     target_corners: [B, 8, 2]
@@ -15,7 +15,7 @@ def corner_regression_loss(pred,target):
     loss = torch.sum(diff) / denom
     return loss
 
-def corner_confidence_loss(pred,target):
+def corner_confidence_loss(pred_conf_logits, target_vis):
     """
     pred_conf_logits: [B, 8]
     target_vis:       [B, 8]
@@ -34,4 +34,8 @@ def corner_loss(pred_corners, target_corners, pred_conf_logits, target_vis):
     """
     reg_loss = corner_regression_loss(pred_corners, target_corners, target_vis)
     conf_loss = corner_confidence_loss(pred_conf_logits, target_vis)
-    return reg_loss + conf_loss
+    return {
+        "loss": loss,
+        "loss_corner": loss_corner,
+        "moss_conf": loss_conf
+    }
